239f74bf9e1b7c0a572eb29eb8bb7cbd65f5f4c2
JvillapEdu <Jvillap@eafit.edu.co>
2026-09-23 22:01:48 -0500

    Fix image filename in Intro.py

--- Intro.py
+++ Intro.py
@@ -47,7 +47,7 @@ with col2:
  st.write(f"[Enlace]({url})")
 
  st.subheader("TF-IDF")
- image = Image.open('text_to_audio.png')
+ image = Image.open('txt_to_audio.png')
  st.image(image, width=190)
  st.write("La siguiente aplicación usa esta medida para evaluar relevancia entre 3 preguntas.") 
  url = "https://tfidfj-mv4mppkzxsnc2bko4ysjfn.streamlit.app/"
